8fbe70798f68b318a2931a5d996ef6079dfcd4f4
Avineet Yadav <12minute00@gmail.com>
2024-08-07 01:03:01 +0530

    add suggestion_api

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -6,13 +6,12 @@ export const USER_ICON = `https://cdn-icons-png.freepik.com/256/1077/1077114.png
 
 export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=`;
 
-export const YOUTUBE_SEARCH_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const YOUTUBE_SEARCH_API = "https://auto-suggestion-api-git-main-avineet-yadavs-projects.vercel.app/search?q=";
 
+export const YOUTUBE_COMMENT_API = `https://www.googleapis.com/youtube/v3/commentThreads?key=${process.env.REACT_APP_GOOGLE_API_KEY}&textFormat=plainText&part=snippet,replies&maxResults=10&videoId=`;
 
-export const YOUTUBE_COMMENT_API = `https://www.googleapis.com/youtube/v3/commentThreads?key=${process.env.REACT_APP_GOOGLE_API_KEY}&textFormat=plainText&part=snippet,replies&maxResults=10&videoId=`
- 
-export const SEARCH_SUGGESTION_API = `https://www.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_GOOGLE_API_KEY}&part=snippet&maxResults=10&type=video&q=`
+export const SEARCH_SUGGESTION_API = `https://www.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_GOOGLE_API_KEY}&part=snippet&maxResults=10&type=video&q=`;
 
-export const YOUTUBE_CHANNEL_API = `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics,contentDetails&key=${process.env.REACT_APP_GOOGLE_API_KEY}&id=`
+export const YOUTUBE_CHANNEL_API = `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics,contentDetails&key=${process.env.REACT_APP_GOOGLE_API_KEY}&id=`;
 
 export const OFFSET_LIVE_CHAT = 10;
