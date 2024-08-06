5009ad20cd35d5efcb590df156f255f6b0a3d334
Avineet Yadav <12minute00@gmail.com>
2024-08-07 01:08:16 +0530

    add suggestion_api

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -6,7 +6,7 @@ export const USER_ICON = `https://cdn-icons-png.freepik.com/256/1077/1077114.png
 
 export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=`;
 
-export const YOUTUBE_SEARCH_API = "lkasdf";
+export const YOUTUBE_SEARCH_API = "https://auto-suggestion-api-git-main-avineet-yadavs-projects.vercel.app/search?q=";
 
 export const YOUTUBE_COMMENT_API = `https://www.googleapis.com/youtube/v3/commentThreads?key=${process.env.REACT_APP_GOOGLE_API_KEY}&textFormat=plainText&part=snippet,replies&maxResults=10&videoId=`;
 
