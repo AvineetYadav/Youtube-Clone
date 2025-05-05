90033d1c0b3c8f2c4b60d318e86f12e00d44eac0
Avineet Yadav <12minute00@gmail.com>
2025-05-05 19:39:52 +0530

    edit

--- src/Components.js/ChatMessage.js
+++ src/Components.js/ChatMessage.js
@@ -4,7 +4,7 @@ import { USER_ICON } from "../utils/constant";
 const ChatMessage = ({ name, message }) => {
   return (
     <div className="flex items-center shadow-sm p-2">
-      <img className="h-8" src={USER_ICON} />
+      <img className="h-8" src={USER_ICON} alt="UserICON" />
       <span className="font-bold  px-4">{name}</span>
       <span>{message}</span>
     </div>
